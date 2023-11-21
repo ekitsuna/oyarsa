cb6a516246d082d890fd1dbdc7cd401b185e0f2c
ekistuna <arthurca3607@gmail.com>
2023-11-21 17:07:47 -0600

    holo

--- Items/TestSword.cs
+++ Items/TestSword.cs
@@ -14,7 +14,7 @@ namespace depths.Items
 
 		public override void SetDefaults()
 		{
-			Item.damage = 50;
+			Item.damage = 100;
 			Item.DamageType = DamageClass.Melee;
 			Item.width = 40;
 			Item.height = 40;
